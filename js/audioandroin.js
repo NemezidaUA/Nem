2ba6868a7cd7506e9241be24b87851dd7c529b48
SergeyMuff <50599876+SergeyMuff@users.noreply.github.com>
2019-06-22 19:17:54 +0300

    7

--- js/audioandroin.js
+++ js/audioandroin.js
@@ -1,1 +1,13 @@
-mWebView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+var audio = document.getElementsByTagName('audio')[0],
+    button = document.getElementsByTagName('button')[0];
+
+audio.onclick = playAudio;
+button.onclick = playAudio;
+  
+function playAudio() {
+  if (audio.paused) {
+    audio.play();
+  } else {
+     audio.pause();
+  }
+};
